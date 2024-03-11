7d86af6fc065be6ba0ceac841f885c88d6761a77
nishcreed <nishaanth27rams@gmail.com>
2024-03-11 17:57:04 +0530

    deployment

--- server/app.js
+++ server/app.js
@@ -170,7 +170,7 @@ function handleDisconnect(userId) {
 const server = http.createServer((req,res) => {
   // Allow preflight requests
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
